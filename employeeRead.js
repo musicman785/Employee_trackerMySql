5c0d93d43b46e6b909019c749280ef4e956e044d
Chris Salgado <musicman785@gmail.com>
2020-06-18 16:22:58 -0700

    Created function to view employees by department

--- employeeRead.js
+++ employeeRead.js
@@ -4,57 +4,57 @@ const table = require("console.table")
 
 
 const connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "Rudim3nts08",
-    database: "employeeTracker_db"
-  });
+  host: "localhost",
 
-  // connect to the mysql server and sql database
-connection.connect(function(err) {
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Rudim3nts08",
+  database: "employeeTracker_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(err => {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   ask();
 });
-  
+
 
 function ask() {
-    inquirer
-      .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-          "View All Employees",
-          "View All Employees By Department",
-          "View All Employees By Manager",
-          "Add Employee",
-          "Remove Employee",
-          "Update Employee Role",
-          "Update Employee Manager"
-        ]
-      })
-      .then(function(answer) {
-        switch (answer.action) {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager"
+      ]
+    })
+    .then(answer => {
+      switch (answer.action) {
         case "View All Employees":
           employeesAll();
           break;
-  
+
         case "View All Employees By Department":
           department();
           break;
-  
+
         case "View All Employees By Manager":
           manager();
           break;
-  
+
         case "Add Employee":
           add();
           break;
@@ -65,34 +65,56 @@ function ask() {
 
         case "Update Employee Role":
           updateRole();
-           break;
+          break;
 
         case "Update Employee Manager":
           updateManager();
           break;
-            
+
         case "exit":
           connection.end();
           break;
 
-          
-        }
-      });
-  } 
-  const employeesAll = () => {
-     connection.query(`SELECT employee.id, first_name, last_name, title, dept, salary, manager_id
-     FROM employee
-     INNER JOIN role
-     ON employee.dept_id = role.id
-     INNER JOIN department
-     ON role.department_id = department.id`, (err, res) =>{
-       if (err) throw err;
-      console.table(res);
-      ask();
-    })
-  };
 
-  // Need function to say that if user selects VIEW ALL EMPLOYEES, then show table of employees with all values. (id, first_name, last_name, role, department, salary, manager) 
+      }
+    });
+}
+const tableAll = (`SELECT employee.id, first_name, last_name, title, dept, salary, manager_id
+FROM employee
+INNER JOIN role
+ON employee.dept_id = role.id
+INNER JOIN department
+ON role.department_id = department.id`);
+
+const employeesAll = () => {
+  connection.query(tableAll, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    ask();
+  })
+};
+
+const department = () => {
+  inquirer.prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "Engineering",
+      "Finance",
+      "Legal",
+      "Sales",
+     ]
+  }).then(response => {
+    connection.query(`${tableAll}
+    WHERE dept = "${response.action}"`, (err, res) => {
+    console.table(res);
+    ask();
+    })
+  })
+
+}
+  // Need function to say that if user selects VIEW ALL EMPLOYEES, then show table of employees with all values. (id, first_name, last_name, role, department, salary, manager)
 
    // Need function to say that if user selects VIEW EMPLOYEES BY DEPARTMENT, then ask question about what department, then return list for all employees under selected department
 
